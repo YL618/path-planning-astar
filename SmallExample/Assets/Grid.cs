56f6c3d622c5a833ba8c2a77414f0d1deb712008
LOYOJO990618 <52713987+LOYOJO990618@users.noreply.github.com>
2019-07-24 04:58:05 +0800

    Initial

---
+++ SmallExample/Assets/Grid.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Grid : MonoBehaviour
+{
+
+    public bool displayGridGizmos;
+    public LayerMask unwalkableMask;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    public Node[,] grid;
+    public List<Node> Apath;
+    float nodeDiameter;
+    int gridSizeX, gridSizeY;
+
+    void Awake()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+        Debug.Log("Creat Finish!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1");
+    }
+
+    public int MaxSize
+    {
+        get
+        {
+            return gridSizeX * gridSizeY;
+        }
+    }
+
+    void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+                Vector3 halfbox;
+                halfbox.x = nodeRadius;
+                halfbox.y = nodeRadius;
+                halfbox.z = nodeRadius;
+                //Quaternion qua;
+                //qua = Quaternion.Euler(0, 0, 0);
+                //bool walkable = !(Physics.CheckBox(worldPoint, halfbox, transform.rotation, unwalkableMask));
+                bool walkable = !(Physics.CheckBox(worldPoint, halfbox, transform.rotation, unwalkableMask));
+                grid[x, y] = new Node(walkable, worldPoint, x, y);
+               
+            }
+        }
+        Debug.Log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! GRID LENGTH !!!!!!!!!!!!!!!!!!!!!!!!!!");
+        Debug.Log(grid.Length);
+        Debug.Log(gridSizeX * gridSizeY);
+        //OnDrawGizmos();
+    }
+
+    public void CheckUnwalkable()
+    {
+        //Debug.Log("8888888888888888888888   CheckUnwalkable   88888888888888888888888888888888888888888");
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+                Vector3 halfbox;
+                halfbox.x = nodeRadius;
+                halfbox.y = nodeRadius;
+                halfbox.z = nodeRadius;
+                //Quaternion qua;
+                //qua = Quaternion.Euler(0,0,0);
+                //bool walkable = !(Physics.CheckBox(worldPoint, halfbox, transform.rotation, unwalkableMask));
+                bool walkable = !(Physics.CheckBox(worldPoint, halfbox, transform.rotation, unwalkableMask));
+                grid[x, y] = new Node(walkable, worldPoint, x, y);
+
+            }
+        }
+    }
+    public List<Node> GetNeighbours(Node node)
+    {
+        List<Node> neighbours = new List<Node>();
+
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                if (x == 0 && y == 0)
+                    continue;
+
+                int checkX = node.gridX + x;
+                int checkY = node.gridY + y;
+
+                if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
+                {
+                    neighbours.Add(grid[checkX, checkY]);
+                }
+            }
+        }
+
+        return neighbours;
+    }
+
+    public Node NodeFromWorldPoint(Vector3 worldPosition)
+    {
+        //Debug.Log("=======================NodeFromWorldPoint============================================");
+        float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
+        float percentY = (worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
+        percentX = Mathf.Clamp01(percentX);
+        percentY = Mathf.Clamp01(percentY);
+
+        int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+        int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+        //Debug.Log("|||||||||||||||||||||||||||  FINISH NodeFromWorldPoint ||||||||||||||||||||||||||||||");
+        return grid[x, y];
+    }
+
+    //void OnDrawGizmos()
+    //{
+    //    Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+    //    if (grid != null )//&& displayGridGizmos)
+    //    {
+    //        foreach (Node n in grid)
+    //        {
+    //            Gizmos.color = (n.walkable) ? Color.white : Color.red;
+    //            Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
+    //        }
+    //    }
+    //}
+}
---
+++ SmallExample/Assets/Unit.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+//using System.Diagnostics;
+public class Unit : MonoBehaviour
+{
+    public Transform target;
+    public LayerMask unwalkableMask;
+    List<Vector3> relative;
+    List<Node> path = new List<Node>();
+    public Vector3[] waypoints;
+    //Vector3[] path;
+    Grid _grid;
+    Node gridNode;
+    Grid Agrid;
+    bool pathSuccess = false;
+    float speed = 20;
+    int targetIndex;
+    int relX;
+    int relY;
+    int bigger;
+    int size ;
+
+    
+        //set waypoints for the obj
+        
+    public void OnDrawGizmos()
+    {
+        if (waypoints != null)
+        {
+            for (int i = targetIndex; i < waypoints.Length; i++)
+            {
+                Gizmos.color = Color.black;
+                Gizmos.DrawCube(waypoints[i], Vector3.one);
+
+                if (i == targetIndex)
+                {
+                    Gizmos.DrawLine(transform.position, waypoints[i]);
+                }
+                else
+                {
+                    Gizmos.DrawLine(waypoints[i - 1], waypoints[i]);
+                }
+            }
+        }
+    }
+}
